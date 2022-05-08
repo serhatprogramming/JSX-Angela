eb45d9b01ad04d95a1139274a09c5a211a66ed63
serhatprogramming <92653297+serhatprogramming@users.noreply.github.com>
2022-05-08 14:35:37 -0400

    createRoot function added.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
+import React from "react";
+import ReactDOM from "react-dom/client";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
+// const React = require("react");
+// const ReactDOM = require("react-dom");
+root.render(
+  <div>
+    <h1>Hello World</h1>
+    <p>This is a paragraph</p>
+  </div>
+);
